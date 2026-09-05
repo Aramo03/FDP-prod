0df7c8b7d10ed7a17a18c5080089334c137a141d
Aram <aram1023aram@gmail.com>
2026-09-05 10:31:15 +0400

    Add Django test for Task model

--- backend/tasks/tests.py
+++ backend/tasks/tests.py
@@ -1,2 +1,13 @@
-__pycache__/
-*.py[cod]
+from django.test import TestCase
+from .models import Task
+
+
+class TaskTestCase(TestCase):
+    def test_create_task(self):
+        task = Task.objects.create(
+            title="Test task",
+            description="Testing CI",
+        )
+
+        self.assertEqual(task.title, "Test task")
+        self.assertFalse(task.completed)      
